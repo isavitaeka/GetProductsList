c6a66ac5075c2f23719634880db6dcb9ed8f5a96
Savita Savita <savita.singh3@vodafone.com>
2024-02-07 16:37:46 +0000

    adding files

---
+++ index.py
@@ -0,0 +1,13 @@
+import json
+
+#with open('products.txt', 'r') as f:
+#    data = json.load(f)
+#print(data)
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Again This is the next change I am going GetProductList Lambda....!')
+ #       'body' : json.dumps(data)
+    }
