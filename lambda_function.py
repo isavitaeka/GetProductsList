c2b2577b0c1b4f3eb03e2548b9924332e5d633d4
Savita Savita <savita.singh3@vodafone.com>
2024-02-07 15:21:30 +0000

    adding files

--- lambda_function.py
+++ lambda_function.py
@@ -8,6 +8,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Again hcnage This is the next change I am going GetProductList Lambda....!')
+        'body': json.dumps('Again This is the next change I am going GetProductList Lambda....!')
  #       'body' : json.dumps(data)
     }
